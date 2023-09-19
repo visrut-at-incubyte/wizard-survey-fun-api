2fc906d1b6369c6c7fd7b6ea6a8027ef92a7639e
Visrut <visrut@incubyte.co>
2023-09-19 22:00:31 +0530

    converted to es module

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require("express");
+import express, { json } from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(json());
 
 app.get("/", (req, res) => {
-  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  const forwardedFor = req.headers["x-forwarded-for"];
+  const ip = forwardedFor
+    ? forwardedFor.split(",")[0].trim()
+    : req.connection.remoteAddress;
   res.json({ ip: ip });
 });
 
